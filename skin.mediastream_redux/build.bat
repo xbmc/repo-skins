3bd8c3fe5345f28e4171cae90937613e7b45d823
Cory Fields <theuni-nospam-@xbmc.org>
2010-06-06 22:07:28 -0400

    sync pm3-hd and mediastream_redux with upstream r46

---
+++ skin.mediastream_redux/build.bat
@@ -0,0 +1,33 @@
+@echo off
+ECHO ----------------------------------------
+echo Creating Build Folder
+rmdir BUILD /S /Q
+md BUILD\skin.mediastream_redux\media\
+
+Echo .svn>exclude.txt
+Echo Thumbs.db>>exclude.txt
+Echo Desktop.ini>>exclude.txt
+Echo dsstdfx.bin>>exclude.txt
+Echo exclude.txt>>exclude.txt
+
+ECHO ----------------------------------------
+ECHO Creating XBT File...
+START /B /WAIT ..\..\TexturePacker\TexturePacker -input media -output BUILD\skin.mediastream_redux\media\Textures.xbt
+
+ECHO ----------------------------------------
+ECHO XBT Texture Files Created...
+ECHO Building Skin Directory...
+xcopy "720p" "BUILD\skin.mediastream_redux\720p" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "fonts" "BUILD\skin.mediastream_redux\fonts" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "backgrounds" "BUILD\skin.mediastream_redux\backgrounds" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "sounds\*.*" "BUILD\skin.mediastream_redux\sounds\" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "colors\*.*" "BUILD\skin.mediastream_redux\colors\" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "language" "BUILD\skin.mediastream_redux\language" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "scripts" "BUILD\skin.mediastream_redux\scripts" /E /Q /I /Y /EXCLUDE:exclude.txt
+
+del exclude.txt
+
+copy *.xml "BUILD\skin.mediastream_redux\"
+copy *.txt "BUILD\skin.mediastream_redux\"
+copy icon.png "BUILD\skin.mediastream_redux\"
+pause
---
+++ skin.pm3-hd/build.bat
@@ -0,0 +1,32 @@
+@echo off
+ECHO ----------------------------------------
+echo Creating Build Folder
+rmdir BUILD /S /Q
+md BUILD\skin.pm3.hd\media\
+
+Echo .svn>exclude.txt
+Echo Thumbs.db>>exclude.txt
+Echo Desktop.ini>>exclude.txt
+Echo dsstdfx.bin>>exclude.txt
+Echo exclude.txt>>exclude.txt
+
+ECHO ----------------------------------------
+ECHO Creating XBT File...
+START /B /WAIT ..\..\TexturePacker\TexturePacker -input media -output BUILD\skin.pm3.hd\media\Textures.xbt
+
+ECHO ----------------------------------------
+ECHO XBT Texture Files Created...
+ECHO Building Skin Directory...
+xcopy "720p" "BUILD\skin.pm3.hd\720p" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "fonts" "BUILD\skin.pm3.hd\fonts" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "backgrounds" "BUILD\skin.pm3.hd\backgrounds" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "sounds\*.*" "BUILD\skin.pm3.hd\sounds\" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "colors\*.*" "BUILD\skin.pm3.hd\colors\" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "language" "BUILD\skin.pm3.hd\language" /E /Q /I /Y /EXCLUDE:exclude.txt
+
+del exclude.txt
+
+copy *.xml "BUILD\skin.pm3.hd\"
+copy *.txt "BUILD\skin.pm3.hd\"
+copy icon.png "BUILD\skin.pm3.hd\"
+pause
